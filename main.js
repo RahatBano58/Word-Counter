85366b18c37fcbe9c80b05c1dfec934683516b22
Rahat Bano <160172836+RahatBano58@users.noreply.github.com>
2024-05-06 20:58:48 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+//import the 'inquirer' module, which is command line interface for Node.js
+import inquirer from "inquirer";
+//Declear a constant 'answers' and assign it to the result of inquirer.prompt function
+const answers = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: ("Enter your sentence to count the word:")
+    }
+]);
+const words = answers.sentence.trim().split(" ");
+// Print the array of words to the console 
+console.log(words);
+// Print the word count of the sentence to the console
+console.log(`your sentence word count is ${words.length}`);
